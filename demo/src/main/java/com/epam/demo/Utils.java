9f4b0ab448936f5db65bc228b87d0573082f2201
rtemirbulat <equalityisnotequality@gmail.com>
2024-04-11 01:22:01 +0500

    third

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,11 @@
 package com.epam.demo;
 
+import org.apache.commons.lang3.StringUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
-        return args.stream().allMatch(s->Double.parseDouble(s)>0);
+        return args.stream().allMatch(s-> StringUtils.isNumeric(s) && Double.parseDouble(s)>0);
     }
 }
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,6 @@
 package com.epam.utils;
-
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return Integer.parseInt(str) > 0;
+        return Integer.parseInt(str) > 0 && org.apache.commons.lang3.StringUtils.isNumeric(str);
     }
 }
